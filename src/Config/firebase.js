f1c5bce0967f7941d9d043719de677dafb2b6dab
Mateen-Mazhar <mateenmazhar16@gmail.com>
2025-01-28 23:54:16 +0500

    firebase confidentials hidden

--- src/Config/firebase.js
+++ src/Config/firebase.js
@@ -9,13 +9,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBoUuTGVQtzftSD0m_dpoDeDLc33_5KUz0",
-  authDomain: "notes-app-7e13c.firebaseapp.com",
-  projectId: "notes-app-7e13c",
-  storageBucket: "notes-app-7e13c.firebasestorage.app",
-  messagingSenderId: "96443279263",
-  appId: "1:96443279263:web:73c6616cbd8a0af0bebfaa",
-  measurementId: "G-0MNV3CTL78",
+  apiKey: process.env.REACT_APP_FIREBASE_apiKey,
+  authDomain: process.env.REACT_APP_FIREBASE_authDomain,
+  projectId: process.env.REACT_APP_FIREBASE_projectId,
+  storageBucket: process.env.REACT_APP_FIREBASE_storageBucket,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_messagingSenderId,
+  appId: process.env.REACT_APP_FIREBASE_appId,
+  measurementId: process.env.REACT_APP_FIREBASE_measurementId,
 };
 
 // Initialize Firebase
